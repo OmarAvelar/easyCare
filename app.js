49b0ecf6b93afa50efc30d3de1c0acc5c6dddbc7
Omar Avelar <omar.avelar.f@gmail.com>
2018-12-06 19:26:03 -0600

    lol

--- app.js
+++ app.js
@@ -44,62 +44,62 @@ const express = require('express');
    });
  });
  
-//  passport.use('local-login', new LocalStrategy((username, password, next) => {
-//    User.findOne({ username }, (err, user) => {
-//      if (err) {
-//        return next(err);
-//      }
-//      if (!user) {
-//        return next(null, false, { message: "Incorrect username" });
-//      }
-//      if (!bcrypt.compareSync(password, user.password)) {
-//        return next(null, false, { message: "Incorrect password" });
-//      }
+ passport.use('local-login', new LocalStrategy((username, password, next) => {
+   User.findOne({ username }, (err, user) => {
+     if (err) {
+       return next(err);
+     }
+     if (!user) {
+       return next(null, false, { message: "Incorrect username" });
+     }
+     if (!bcrypt.compareSync(password, user.password)) {
+       return next(null, false, { message: "Incorrect password" });
+     }
  
-//      return next(null, user);
-//    });
-//  }));
+     return next(null, user);
+   });
+ }));
 
-//  passport.use('local-signup', new LocalStrategy(
-//    { passReqToCallback: true },
-//    (req, username, p, next) => {
-//      // To avoid race conditions
-//      process.nextTick(() => {
-//          User.findOne({
-//           'username': username
-//          }, (err, user) => {
-//              if (err){ return next(err); }
+ passport.use('local-signup', new LocalStrategy(
+   { passReqToCallback: true },
+   (req, username, p, next) => {
+     // To avoid race conditions
+     process.nextTick(() => {
+         User.findOne({
+          'username': username
+         }, (err, user) => {
+             if (err){ return next(err); }
  
-//              if (user) {
-//                  return next(null, false);
-//              } else {
-//                  // Destructure the body
-//                  const {
-//                    username,
-//                    email,
-//                    password,
-//                    role,
-//                  } = req.body;
-//                  //const path = `/uploads/${req.file.filename}`;
-//                  //const {originalname} = req.file
-//                  const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-//                  const newUser = new User({
-//                    username,
-//                    email,
-//                    password: hashPass,
-//                    role,
-//                    //profilePicture:{path, originalname}
-//                  });
+             if (user) {
+                 return next(null, false);
+             } else {
+                 // Destructure the body
+                 const {
+                   username,
+                   email,
+                   password,
+                   role,
+                 } = req.body;
+                 //const path = `/uploads/${req.file.filename}`;
+                 //const {originalname} = req.file
+                 const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+                 const newUser = new User({
+                   username,
+                   email,
+                   password: hashPass,
+                   role,
+                   //profilePicture:{path, originalname}
+                 });
 
-//                  newUser.save((err) => {
-//                   welcomeMail(newUser.username, newUser.email)
-//                      if (err){ next(null, false, { message: newUser.errors }) }
-//                      return next(null, newUser);
-//                  });
-//              }
-//          });
-//      });
-//  }));
+                 newUser.save((err) => {
+                  welcomeMail(newUser.username, newUser.email)
+                     if (err){ next(null, false, { message: newUser.errors }) }
+                     return next(null, newUser);
+                 });
+             }
+         });
+     });
+ }));
  
  app.use(flash());
  app.use(passport.initialize());
